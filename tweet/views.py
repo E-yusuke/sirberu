aaef81eff949d6d27787e295966facaffbd1cef8
yusuke <suke15yoshi@gmail.com>
2020-11-20 08:43:43 +0900

    add the frontend

--- tweet/views.py
+++ tweet/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Post
+
 
 class PostListView(ListView):
     model = Post
@@ -8,4 +9,9 @@ class PostListView(ListView):
     ordering = ['-date_posted']
     paginate_by = 5
 
-# Create your views here.
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['content']
+    template_name = 'tweet/post_create.html'
+    success_url = '/'
